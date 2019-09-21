b91a7a00fa652882c4e1691d1fe4ba802372ee2b
Magomed Khamidov <khamidov.che@gmail.com>
2019-09-21 17:21:21 +0300

    Was added changes to layout of views and home view.

--- resources/views/home/index.php
+++ resources/views/home/index.php
@@ -1,1 +1,63 @@
 <?php
+/** @var array $data */
+?>
+<div class="card">
+  <div class="card-header">
+    <div class="d-flex align-items-center">
+      <h3 class="mr-auto card-title">Задачи</h3>
+      <div class="btn-group" role="group">
+        <a href="#" class="btn btn-outline-primary">Новая задача</a>
+      </div>
+    </div>
+  </div>
+  <div class="card-body p-0">
+    <table class="table">
+      <thead class="thead-dark">
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">Имя пользователя</th>
+        <th scope="col">Email</th>
+        <th scope="col">Статус</th>
+        <th scope="col">Текст</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr>
+        <th scope="row">1</th>
+        <td>Mark</td>
+        <td>Otto</td>
+        <td>@mdo</td>
+        <td>@mdo</td>
+      </tr>
+      <tr>
+        <th scope="row">2</th>
+        <td>Jacob</td>
+        <td>Thornton</td>
+        <td>@fat</td>
+        <td>@fat</td>
+      </tr>
+      <tr>
+        <th scope="row">3</th>
+        <td>Larry</td>
+        <td>the Bird</td>
+        <td>@twitter</td>
+        <td>@twitter</td>
+      </tr>
+      </tbody>
+    </table>
+  </div>
+  <div class="card-footer">
+    <div class="row">
+      <div class="col-lg-4">
+        <div class="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups">
+          <div class="btn-group mr-2" role="group" aria-label="First group">
+            <button type="button" class="btn btn-secondary">1</button>
+            <button type="button" class="btn btn-secondary">2</button>
+            <button type="button" class="btn btn-secondary">3</button>
+            <button type="button" class="btn btn-secondary">4</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
