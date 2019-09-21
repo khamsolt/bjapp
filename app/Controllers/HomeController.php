fcb65e7fc600a039af52f77a9244f1fb013d66cb
Magomed Khamidov <khamidov.che@gmail.com>
2019-09-21 17:16:52 +0300

    Added view management class.

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -4,10 +4,27 @@
 namespace App\Controllers;
 
 
+use App\View\Manager as View;
+use MysqliDb;
+use Pecee\Http\Request;
+use Pecee\SimpleRouter\Router;
+
 class HomeController
 {
+    private $database;
+    private $router;
+    private $request;
+
+    public function __construct(MysqliDb $database, Router $router, Request $request)
+    {
+        $this->database = $database;
+        $this->router = $router;
+        $this->request = $request;
+    }
+
     public function index()
     {
-        return 'Home controller : index action <br>';
+        $data = ['Magomed', 'Khamidov', 'Developer'];
+        return View::make('index', 'home')->with('data', $data);
     }
 }
--- app/View/Manager.php
+++ app/View/Manager.php
@@ -6,15 +6,88 @@ namespace App\View;
 
 class Manager
 {
-    private $view = '';
+    private $layoutDir = 'layout';
+    private $viewName;
+    private $viewPath;
+    private $data;
     private $layout = 'app';
-    private $path = ROOT . '/resource/views';
+    private $path = ROOT . '/resources/views';
 
-    public function __construct()
+    /**
+     * Manager constructor.
+     * @param string $viewName
+     * @param string $ViewPath
+     */
+    public function __construct(string $viewName, string $ViewPath)
     {
+        $this->data = [];
+        $this->viewName = $viewName;
+        $this->viewPath = $ViewPath;
     }
 
-    public static function make(string $name)
+    /**
+     * @param string $viewName
+     * @param string $ViewPath
+     * @return static
+     */
+    public static function make(string $viewName, string $ViewPath): self
     {
+        $instance = new self($viewName, $ViewPath);
+        return $instance;
+    }
+
+    /**
+     * @param string $key
+     * @param $data
+     */
+    public function with(string $key, $data): self
+    {
+        $this->data[$key] = $data;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function __toString(): string
+    {
+        return $this->makeTemplate();
+    }
+
+    /**
+     * @return false|string
+     */
+    private function makeTemplate()
+    {
+        if (is_file($layout = $this->makeLayoutPath())) {
+            ob_start();
+            if (is_file($view = $this->makeViewPath())) {
+                if (count($this->data)) {
+                    extract($this->data);
+                }
+                include_once $view;
+                $content = ob_get_clean();
+            }
+            ob_start();
+            include_once $layout;
+            return ob_get_clean();
+        }
+        return '';
+    }
+
+    /**
+     * @return string
+     */
+    private function makeLayoutPath(): string
+    {
+        return $path = $this->path . DIRECTORY_SEPARATOR . $this->layoutDir . DIRECTORY_SEPARATOR . $this->layout . '.php';
+    }
+
+    /**
+     * @return string
+     */
+    private function makeViewPath(): string
+    {
+        return $this->path . DIRECTORY_SEPARATOR . $this->viewPath . DIRECTORY_SEPARATOR . $this->viewName . '.php';
     }
 }
