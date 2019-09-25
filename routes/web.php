ed5bc7e7b9218d24dd5e283659f5781272259e50
Magomed Khamidov <khamidov.che@gmail.com>
2019-09-25 07:53:07 +0300

    Added name a home to route home. Added new route add.

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,9 @@ use Pecee\SimpleRouter\SimpleRouter;
 
 SimpleRouter::setDefaultNamespace('\App\Controllers');
 
-SimpleRouter::get('/', 'HomeController@index', []);
+SimpleRouter::get('/', 'HomeController@index')->name('home');
+
+SimpleRouter::post('/add', 'TaskController@store')->name('add');
 
 SimpleRouter::get('/admin', 'AdminController@index', []);
 
